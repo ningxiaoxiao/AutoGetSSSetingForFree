5418e568186fba47aca441f373ee99fffda5a830
ningxiaoxiao <732869825@qq.com>
2017-06-15 15:17:13 +0800

    添加项目文件。

---
+++ AutoGetSSSetingForFree/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace AutoGetSSSetingForFree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //下载图片
+
+            var req = WebRequest.Create("http://get.shadowsocks8.cc/images/server03.png");
+            var res = req.GetResponse();
+            var result = new StreamReader(res.GetResponseStream(), Encoding.UTF8);
+
+
+
+        }
+    }
+}
